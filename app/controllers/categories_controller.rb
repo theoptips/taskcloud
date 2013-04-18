0aca5fb3dc6507f8edb26222da575afac2ed8135
Dilys Sun <optanovo@gmail.com>
2013-04-18 13:55:18 -0700

    imp 3 basic column logic with randomness

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,17 @@ class CategoriesController < ApplicationController
   def show
   	if sign_in?
       @tasks = Task.where("user_id = #{session[:user_id]}").order("created_at DESC").find_all_by_category_id(params[:id])
+      @columns = Array.new()
+      @columns[0] = Array.new()
+      @columns[1] = Array.new()
+      @columns[2] = Array.new()
+      @tasks.each do |task|
+        random = rand(0..2)
+        @columns[random].push(task)
+      end
+
+      # puts @columns
+
       @categories = Category.all
       # render :text => params
     else
