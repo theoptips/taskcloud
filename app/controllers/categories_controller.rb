d152b8d4dcf50b453435ade873573e58c5e95f8c
Dilys Sun <optanovo@gmail.com>
2013-04-18 11:26:07 -0700

    added basic listing logic per user id per category on dashboard

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,12 @@
 class CategoriesController < ApplicationController
   def show
+  	if sign_in?
+      @tasks = Task.where("user_id = #{session[:user_id]}").order("created_at DESC").find_all_by_category_id(params[:id])
+      @categories = Category.all
+      # render :text => params
+    else
+      flash[:error] = "Please sign in first."
+      redirect_to signin_path
+    end
   end
 end
